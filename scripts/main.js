55d4283e39632f8e8a5e7dcfb4c954a57ddd287a
Jim Fleming <jimmy.fleming@gmail.com>
2016-01-01 14:47:29 -0800

    Add Two.js for rendering

--- scripts/main.js
+++ scripts/main.js
@@ -12,125 +12,66 @@
   var maxDistance = 72; // 64
   var pixelWidth = 2.0; // 0.5
   var maxNeighbors = 6;
-  var avoidWalls = false;
-  var avoidance = 2.0;
-
-  var canvas = document.querySelector("canvas");
-  var ctx = canvas.getContext("2d");
-
-  var devicePixelRatio = window.devicePixelRatio || 1;
-  var backingStoreRatio = ctx.backingStorePixelRatio || 1;
-  var ratio = devicePixelRatio / backingStoreRatio;
 
   var width = window.innerWidth / 2;
   var height = window.innerHeight;
 
-  canvas.width = width * ratio;
-  canvas.height = height * ratio;
-
-  canvas.style.width = width + "px";
-  canvas.style.height = height + "px";
-
-  ctx.scale(ratio, ratio);
-
-  var els = document.querySelectorAll(".avoid");
-
-  function contains(rect, v) {
-    return v.x > rect.left && v.x < rect.right && v.y > rect.top && v.y < rect.bottom;
-  }
-
-  function getCenter(rect) {
-    return new Vector(rect.left + (rect.width / 2), rect.top + (rect.height / 2));
-  }
+  var el = document.getElementById('draw');
+  var params = {
+    width: width,
+    height: height,
+    type: Two.Types.canvas
+  };
+  var two = new Two(params).appendTo(el);
+  var colors = ['#66cc70', '#73c6e5', '#e573b6', '#e5e073', '#e5ac73'];
 
   var boids = [];
+  var boidRenderers = [];
   var position, velocity;
 
-  var particleCount = Math.sqrt(width * height) / 10;
+  var particleCount = Math.floor(Math.sqrt(width * height) / 10);
+  var boid, boidRenderer;
+  var lineGroup = two.makeGroup();
+  var boidGroup = two.makeGroup();
+
   for (var i = 0; i < particleCount; i++) {
     position = new Vector(randomRange(0, width), randomRange(0, height));
     velocity = new Vector(Math.cos(randomRange(0, 2 * Math.PI)), Math.sin(randomRange(0, 2 * Math.PI)));
-    boids.push(new Particle(position, velocity));
+
+    boid = new Particle(position, velocity);
+    boidRenderer = two.makeRectangle(position.x, position.y, pixelWidth * 2, pixelWidth * 2);
+    boidRenderer.fill = colors[i % colors.length];
+    boidRenderer.noStroke();
+    boidGroup.add(boidRenderer);
+
+    boids.push(boid);
+    boidRenderers.push(boidRenderer);
   }
 
-  var rects = [];
-  var boundingRect;
-  for (var i = 0; i < els.length; i++) {
-    boundingRect = els[i].getBoundingClientRect();
-    // cloning to avoid perf hit in bounding rect property access during draw
-    rects.push({
-      left: boundingRect.left,
-      right: boundingRect.right,
-      top: boundingRect.top,
-      bottom: boundingRect.bottom
-    });
-  }
+  var flock = new Flock(boids /* , groups */);
+  var lines = [];
 
-  var flock = new Flock(boids);
-
-  function update() {
+  two.bind('update', function(frameCount) {
     flock.update();
 
-    var boid, rect;
-    for (var i = 0; i < boids.length; i++) {
-      boid = boids[i];
-
-      boid.update();
-
-      for (var j = 0; j < rects.length; j++) {
-        rect = rects[j];
-        if (contains(rect, boid.position)) {
-          boid.acceleration.add(boid.position.clone().sub(getCenter(rect)).normalize().multiplyScalar(avoidance));
-        }
-      }
-
-      if (avoidWalls) {
-        if (boid.position.x < 0) {
-          boid.acceleration.add(new Vector(1, 0).multiplyScalar(avoidance));
-        }
-        if (boid.position.y < 0) {
-          boid.acceleration.add(new Vector(0, 1).multiplyScalar(avoidance));
-        }
-        if (boid.position.x > width) {
-          boid.acceleration.add(new Vector(-1, 0).multiplyScalar(avoidance));
-        }
-        if (boid.position.y > height) {
-          boid.acceleration.add(new Vector(0, -1).multiplyScalar(avoidance));
-        }
-      } else {
-        if (boid.position.x < 0) {
-          boid.position.x = width;
-        }
-        if (boid.position.y < 0) {
-          boid.position.y = height;
-        }
-        if (boid.position.x > width) {
-          boid.position.x = 0;
-        }
-        if (boid.position.y > height) {
-          boid.position.y = 0;
-        }
-      }
-    }
-  }
-
-  var colors = ['#66cc70', '#73c6e5', '#e573b6', '#e5e073', '#e5ac73'];
-
-  function draw() {
     var boidA, boidB;
-    var distance, alpha;
+    var boidRenderer;
+    var opacity, distance;
+    var line;
+    var lineIndex = 0;
 
-    ctx.strokeStyle = "rgb(60, 60, 60)";
-    ctx.lineWidth = pixelWidth;
-    ctx.globalAlpha = 1.0;
-
-    var strokes = {};
+    for (var i = 0; i < lines.length; i++) {
+      lines[i].noStroke();
+    }
 
     for (var i = 0; i < boids.length; i++) {
       boidA = boids[i];
       boidA.neighbors = 0;
+      boidA.update();
+      boidA.wrap(width, height);
 
-      ctx.fillStyle = "rgb(60, 60, 60)";
+      boidRenderer = boidRenderers[i];
+      boidRenderer.translation.set(boidA.position.x, boidA.position.y);
 
       for (var j = 0; j < boids.length; j++) {
         if (i === j) {
@@ -144,60 +85,41 @@
           continue;
         }
 
-        alpha = 1 - (distance / maxDistance);
-
-        // batch strokes by their alpha value
-        alpha = alpha.toFixed(1);
-        if (!strokes[alpha]) {
-          strokes[alpha] = [];
-        }
-        strokes[alpha].push([boidA.position, boidB.position]);
-
         // only draw particles with < max neighbors
         if (boidA.neighbors >= maxNeighbors) {
-          break;
+          continue;
         }
 
+        opacity = (1 - (distance / maxDistance)) * 0.12;
+
+        if (lineIndex < lines.length) {
+          line = lines[lineIndex];
+          line.translation.set(0, 0);
+          line.vertices[line.beginning].set(boidA.position.x, boidA.position.y);
+          line.vertices[line.ending].set(boidB.position.x, boidB.position.y);
+          line.stroke = 'rgb(60, 60, 60)';
+          line.opacity = opacity;
+        } else {
+          line = two.makeLine(boidA.position.x, boidA.position.y, boidB.position.x, boidB.position.y);
+          line.stroke = 'rgb(60, 60, 60)';
+          line.opacity = opacity;
+          line.linewidth = pixelWidth / 2;
+          lineGroup.add(line);
+          lines.push(line);
+        }
+
+        lineIndex++;
         boidA.neighbors++;
       }
 
       if (boidA.neighbors > 0) {
-        ctx.fillStyle = colors[i % colors.length];
-        ctx.fillRect(
-          boidA.position.x - (pixelWidth * 1.0),
-          boidA.position.y - (pixelWidth * 1.0),
-          pixelWidth * 2.0,
-          pixelWidth * 2.0);
+        boidRenderer.fill = colors[i % colors.length];
+      } else {
+        boidRenderer.noFill();
       }
     }
+  });
 
-    var alphaStrokes;
-    var line;
-    var alphas = Object.keys(strokes);
-    for (i = 0; i < alphas.length; i++) {
-      alpha = alphas[i];
-      alphaStrokes = strokes[alpha];
-
-      ctx.globalAlpha = alpha * 0.12;
-
-      ctx.beginPath();
-      for (j = 0; j < alphaStrokes.length; j++) {
-        line = alphaStrokes[j];
-        ctx.moveTo(line[0].x, line[0].y);
-        ctx.lineTo(line[1].x, line[1].y);
-      }
-      ctx.stroke();
-    }
-  }
-
-  function loop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    update();
-    draw();
-
-    requestAnimationFrame(loop);
-  }
-
-  loop();
+  two.play();
 
 })();
--- scripts/particle.js
+++ scripts/particle.js
@@ -5,6 +5,15 @@
 
   var Vector = window.Vector;
 
+  function wrap(x, min, max) {
+    if (x < min) {
+      x = max;
+    } else if (x > max) {
+      x = min;
+    }
+    return x;
+  }
+
   function Particle(point, velocity, acceleration) {
     this.position = point || new Vector(0, 0);
     this.velocity = velocity || new Vector(0, 0);
@@ -18,6 +27,11 @@
     this.position.add(this.velocity);
 
     this.acceleration.multiplyScalar(0);
+  };
+
+  Particle.prototype.wrap = function(xMax, yMax) {
+    this.position.x = wrap(this.position.x, 0, xMax);
+    this.position.y = wrap(this.position.y, 0, yMax);
   };
 
   window.Particle = Particle;
